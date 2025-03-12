0ac06df3adc658b098951f2edfa8d712e28058df
Ayush Gorlawar <140530624+AyushGorlawar@users.noreply.github.com>
2025-03-13 00:41:06 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -49,5 +49,8 @@ def chat():
     
     return jsonify({'response': bot_reply})
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))  # Render ke provided PORT ko use kare, warna default 5000
+    app.run(host="0.0.0.0", port=port)
+
